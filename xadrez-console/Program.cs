4ec500776c195d4d7fde3d0c53ae0b0b2cb9a4dc
Phillip Marques <phillipmarq@hotmail.com>
2023-02-28 00:26:55 -0300

    First tests of piece movement

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,16 +10,22 @@ namespace xadrez_console
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 2));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosition();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosition();
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
+                    partida.executaMovimento(origem, destino);
 
 
-                Tela.imprimirTabuleiro(tab);
+                }
             }
             catch (TabuleiroException ex)
             {
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,5 +1,6 @@
 using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -46,6 +47,14 @@ namespace xadrez_console
                 Console.Write(peca);
                 Console.ForegroundColor = aux;
             }
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
     }
 }
