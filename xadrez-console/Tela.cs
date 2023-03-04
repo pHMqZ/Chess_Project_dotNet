28483b60394c281fdf4f2e0a8e027b6c7ec986da
Phillip Marques <phillipmarq@hotmail.com>
2023-03-04 10:19:30 -0300

    Implementing Check

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -86,6 +86,7 @@ namespace xadrez_console
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            if(partida.xeque) { Console.WriteLine("XEQUE!"); }
         }
 
         private static void imprimirPecasCapturadas(PartidaDeXadrez partida)
